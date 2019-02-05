f4ddaf2765a69f89cfbc70353b3a36c3e0ad10c0
Sebastian <werner@tu-berlin.de>
2019-02-05 10:27:22 +0100

    fixed artifact build issue & updated deployment

--- Jenkinsfile
+++ Jenkinsfile
@@ -13,7 +13,7 @@ pipeline {
             }
         }
         stage('Testing'){
-            options {
+            options {/vdc-agent
                 // Don't need to checkout Git again
                 skipDefaultCheckout true
             }
--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -7,11 +7,19 @@
 ssh -i /opt/keypairs/ditas-testbed-keypair.pem cloudsigma@31.171.247.162 << 'ENDSSH'
 sudo docker stop --time 20 vdc-logging-agent || true
 sudo docker rm --force vdc-logging-agent || true
-sudo docker pull ditas/IMAGE_NAME:v02
+sudo docker pull ditas/vdc-logging-agent:v02
 
 # Get the host IP
 HOST_IP="$(ip route get 8.8.8.8 | awk '{print $NF; exit}')"
 
+echo "{
+    "Port":8484,
+    "ElasticSearchURL":"http://127.0.0.1:9200",
+    "VDCName":"tubvdc",
+    "Endpoint":"http://127.0.0.1:8080",
+    "verbose":false
+}" >> /tmp/la.json
+
 # Run the docker mapping the ports and passing the host IP via the environmental variable "DOCKER_HOST_IP"
-sudo docker run -p 58484:8484 -e DOCKER_HOST_IP=$HOST_IP --restart unless-stopped -d --name vdc-logging-agent ditas/vdc-logging-agent:v02
+sudo docker run -v /tmp/la.json:/.config/logging.json -p 58484:8484 -e DOCKER_HOST_IP=$HOST_IP --restart unless-stopped -d --name vdc-logging-agent ditas/vdc-logging-agent:v02 --testing
 ENDSSH
