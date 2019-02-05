17393091233ad2042d1a7f635c94e16931f95e8b
Sebastian <werner@tu-berlin.de>
2019-02-05 10:27:35 +0100

    updated deployment

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -7,6 +7,7 @@
 ssh -i /opt/keypairs/ditas-testbed-keypair.pem cloudsigma@31.171.247.162 << 'ENDSSH'
 sudo docker stop --time 20 vdc-logging-agent || true
 sudo docker rm --force vdc-logging-agent || true
+sudo docker rmi  ditas/vdc-logging-agent:v02
 sudo docker pull ditas/vdc-logging-agent:v02
 
 # Get the host IP
@@ -21,5 +22,5 @@ echo "{
 }" >> /tmp/la.json
 
 # Run the docker mapping the ports and passing the host IP via the environmental variable "DOCKER_HOST_IP"
-sudo docker run -v /tmp/la.json:/.config/logging.json -p 58484:8484 -e DOCKER_HOST_IP=$HOST_IP --restart unless-stopped -d --name vdc-logging-agent ditas/vdc-logging-agent:v02 --testing
+sudo docker run -v /tmp/la.json:/.config/logging.json -p 58484:8484 -e DOCKER_HOST_IP=$HOST_IP --restart unless-stopped -d --name vdc-logging-agent ditas/vdc-logging-agent:v02 /vdc-agent --testing
 ENDSSH
